e0f127a37ad942320c61ccab84b3e6db8790147b
TheRealSal <75696327+TheRealSal@users.noreply.github.com>
2024-11-11 18:12:49 -0500

    Fixing checkpoint saving code - Code was crashing because parent directory did not exist. Now it creates parent directory as well

--- src/adapters/loading.py
+++ src/adapters/loading.py
@@ -1,7 +1,7 @@
 import json
 import logging
 from abc import ABC, abstractmethod
-from os import mkdir
+from os import mkdir, makedirs
 from os.path import exists, isdir, isfile, join
 from typing import Callable, Mapping, Optional, Sequence, Tuple
 
@@ -78,7 +78,8 @@ class WeightsLoaderHelper:
 
     def save_weights(self, save_directory, filter_func):
         if not exists(save_directory):
-            mkdir(save_directory)
+            #mkdir(save_directory)
+            makedirs(save_directory)
         else:
             assert isdir(save_directory), "Saving path should be a directory where the module weights can be saved."
 
@@ -271,7 +272,8 @@ class WeightsLoader(ABC):
             name (str): An identifier of the weights to be saved. The details are specified by the implementor.
         """
         if not exists(save_directory):
-            mkdir(save_directory)
+            #mkdir(save_directory)
+            makedirs(save_directory)
         else:
             assert isdir(
                 save_directory
@@ -437,7 +439,8 @@ class AdapterLoader(WeightsLoader):
             task_name (str): the name of the adapter to be saved
         """
         if not exists(save_directory):
-            mkdir(save_directory)
+            #mkdir(save_directory)
+            makedirs(save_directory)
         else:
             assert isdir(
                 save_directory
@@ -645,7 +648,8 @@ class AdapterFusionLoader(WeightsLoader):
                 return
 
         if not exists(save_directory):
-            mkdir(save_directory)
+            #mkdir(save_directory)
+            makedirs(save_directory)
         else:
             assert isdir(save_directory), "Saving path should be a directory where the head can be saved."
 
@@ -806,7 +810,8 @@ class PredictionHeadLoader(WeightsLoader):
                 # -> ignore the name and go on
                 name = None
         if not exists(save_directory):
-            mkdir(save_directory)
+            #mkdir(save_directory)
+            makedirs(save_directory)
         else:
             assert isdir(save_directory), "Saving path should be a directory where the head can be saved."
 
